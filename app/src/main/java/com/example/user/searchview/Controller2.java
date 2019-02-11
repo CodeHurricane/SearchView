f1f90e0aa8b85df51f7de2f018033068700355fc
pengfeisun <pengfeisun122@163.com>
2019-02-11 11:22:07 +0800

    first

---
+++ app/src/main/java/com/example/user/searchview/Controller2.java
@@ -0,0 +1,13 @@
+package com.example.user.searchview;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class Controller2 extends BaseController {
+
+	@Override
+	public void draw(Canvas canvas, Paint paint) {
+
+	}
+
+}
---
+++ app/src/main/java/com/example/user/searchview/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.user.searchview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	private MySearchView searchView;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		searchView = (MySearchView)findViewById(R.id.sv);
+		searchView.setController(new Controller1());
+		
+	}
+	
+	public void start(View v){
+		searchView.startAnimation();
+	}
+	public void reset(View v){
+		searchView.resetAnimation();
+	}
+
+}
